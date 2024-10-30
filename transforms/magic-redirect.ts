041ac12b6dba12a43107b2e8b12aa228766fefbf
Filip Kudła <kjugi@users.noreply.github.com>
2024-10-30 01:56:08 +0100

    update magic-redirect codemod transform method and update tests

--- transforms/magic-redirect.ts
+++ transforms/magic-redirect.ts
@@ -1,5 +1,59 @@
-import { API, FileInfo } from "jscodeshift";
+import {
+  API,
+  FileInfo,
+  CallExpression,
+  expressionStatement,
+  logicalExpression,
+  callExpression,
+  literal,
+  memberExpression,
+  identifier,
+  ASTPath,
+} from "jscodeshift";
+import { getParsedFile } from "../utils/parse";
+
+const unifiedMagicString = (path: ASTPath<CallExpression>) => {
+  const pathArguments = path.value.arguments;
+  if (
+    pathArguments.length === 1 &&
+    pathArguments[0].type === "StringLiteral" &&
+    pathArguments[0].value === "back"
+  ) {
+    path.value.arguments = [
+      logicalExpression(
+        "||",
+        callExpression(memberExpression(identifier("req"), identifier("get")), [
+          identifier('"Referrer"'),
+        ]),
+        literal("/")
+      ),
+    ];
+
+    return path;
+  }
+};
 
 export default function transformer(file: FileInfo, _api: API) {
-    return file.source;
+  const parsedFile = getParsedFile(file);
+
+  parsedFile
+    .find(CallExpression, {
+      callee: {
+        property: {
+          name: "redirect",
+        },
+      },
+    })
+    .map(unifiedMagicString);
+  parsedFile
+    .find(CallExpression, {
+      callee: {
+        property: {
+          name: "location",
+        },
+      },
+    })
+    .map(unifiedMagicString);
+
+  return parsedFile.toSource();
 }
